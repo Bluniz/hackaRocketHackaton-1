971e0dbaba348f498173fe6841dafbde0be03965
Wallys L Souza <wallys31.souza@gmail.com>
2020-07-19 09:02:45 -0300

    update

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const server = express()
+
+server.engine('html', require('ejs').renderFile);
+server.use(express.static("public"))
+server.use(express.static("src/views"))
+
+
+server.use(express.urlencoded({ extended: true }))
+
+
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/index", (req, res) => {
+    return res.render("index.html")
+})
+
+
+
+server.get("/cadastro", (req, res) => {
+
+    return res.render("cadastro.html")
+})
+
+server.get("/negocios", (req, res) => {
+
+    return res.render("negocios.html")
+})
+
+server.get("/pizzariamaria", (req, res) => {
+    return res.render("pizzariamaria.html")
+})
+
+server.listen(process.env.PORT || 5000)
